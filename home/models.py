f6159a8c76b7eff262dfd54fe86cfdea25a467ce
Chikayo.K kam <chika000chika@gmail.com>
2024-04-19 22:32:59 +0000

    Add a Reviews model

--- home/models.py
+++ home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 CATEGORY_CHOICES=(0,'Beach'),(1,'History'),(3,'City')
 STATUS=(0,'Draft'),(0,'Published')
+RATE=(0,0),(1,1),(2,2),(3,3),(4,4),(5,5)
 
 class Article(models.Model):
     title = models.CharField(max_length=20, unique=True)
@@ -12,4 +13,13 @@ class Article(models.Model):
     category = models.IntegerField(choices=CATEGORY_CHOICES, default=0)
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
+
+class Reviews(models.Model):
+    article_id=models.ForeignKey
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='chosen_articl')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    review_title = models.CharField(max_length=20)
+    rate = models.IntegerField(choices=RATE, default=0)
+    comment_area = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add= True)
     
--- settings/settings.py
+++ settings/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-68iv+742*a1m03mml8xulao)$wa6%08pn2sgt@gas&)4!dibia'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-chikayok-rateyourstay-k545ie50vak.ws-eu110.gitpod.io','.herokuapp.com']
 

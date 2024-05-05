c1d208ac48bc2486ef28a474fcfa64763df7de40
Chikayo.K kam <chika000chika@gmail.com>
2024-05-05 14:59:46 +0000

    Add category tag and change Explore button colour

--- settings/settings.py
+++ settings/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-68iv+742*a1m03mml8xulao)$wa6%08pn2sgt@gas&)4!dibia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-chikayok-rateyourstay-k545ie50vak.ws-eu110.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-chikayok-rateyourstay-k545ie50vak.ws-eu111.gitpod.io','.herokuapp.com']
 
 
 # Application definition

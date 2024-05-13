f33d58fc1227650dda18a75c081e8ad0d1187611
Chikayo.K kam <chika000chika@gmail.com>
2024-05-13 15:46:32 +0000

    Connect the model to the detaile slug page and render it on the page.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Article
+from .models import Article,Reviews
+
 # Create your views here.
 
 class ArticleList(generic.ListView):
@@ -9,12 +10,17 @@ class ArticleList(generic.ListView):
     paginate_by = 4
 
 def detail(request, slug):
-
+    
     queryset = Article.objects.all()
     detail = get_object_or_404(queryset, slug=slug)
+    reviews = detail.chosen_article.all()
+    
 
     return render(
         request,
         'home/detail.html',
-        {'article':detail},
+        {
+         'article':detail,
+         'reviews':reviews,
+        },
     )

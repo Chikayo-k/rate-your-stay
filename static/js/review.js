f65403faab75b31cc7f51f8a8f62b02245f668ea
Chikayo.K kam <chika000chika@gmail.com>
2024-06-03 01:45:10 +0000

    Added scroll down function when the edit button is clicked, changed the message of the form

--- static/js/review.js
+++ static/js/review.js
@@ -2,11 +2,13 @@ const editBtns = document.getElementsByClassName("edit-btn");
 const reviewText = document.getElementById("id_comment_area");
 const reviewForm = document.getElementById("reviewForm");
 const submitBtn = document.getElementById("submitBtn");
+let editMessage = document.getElementsByClassName("review-leadmessage")[0];
 
 for(let btn of editBtns){
     btn.addEventListener("click",(e)=>{
         let reviewId =  e.target.getAttribute("review_id");
-        let reviewContent = document.getElementById(`review${reviewId}`).innerText;
+        let reviewContent = document.getElementById(`review${reviewId}`).textContent;
+        editMessage.innerHTML= "<p class='fw-bold fs-3'>Edit this meassage!</p>"
         reviewText.value = reviewContent;
         submitBtn.innerText = "Update";
         reviewForm.setAttribute("action", `edit_review/${reviewId}`);

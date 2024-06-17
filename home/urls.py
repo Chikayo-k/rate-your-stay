953e21149beea5503a15a6ee1534dd5a293ad33b
Chikayo Kambara <chika000chika@gmail.com>
2024-06-17 14:18:19 +0000

    Fixed python linter error of urls.py(home)

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
 from . import views
 from django.urls import path
 
-urlpatterns = [   
+urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'),
-    path('<slug:slug>/', views.detail, name='detail'),  
-    path('<slug:slug>/edit_review/<int:review_id>', views.review_edit, name='review_edit'),
-    path('<slug:slug>/review_delete/<int:review_id>', views.review_delete, name='review_delete'),
+    path('<slug:slug>/', views.detail, name='detail'),
+    path('<slug:slug>/edit_review/<int:review_id>', views.review_edit, name='review_edit'),  # noqa
+    path('<slug:slug>/review_delete/<int:review_id>', views.review_delete, name='review_delete'),  # noqa
 ]

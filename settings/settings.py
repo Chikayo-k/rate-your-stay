74a3a2b9f3c2fafd00ae52f53af54e6336f576b6
Chikayo Kambara <chika000chika@gmail.com>
2024-10-22 21:02:32 +0000

    Django secret key is stored in the env.py

--- settings/settings.py
+++ settings/settings.py
@@ -24,11 +24,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-68iv+742*a1m03mml8xulao)$wa6%08pn2sgt@gas&)4!dibia'  # noqa
+SECRET_KEY = os.environ.get("SECRET_KEY")  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['8000-chikayok-rateyourstay-2xv2w027ap3.ws.codeinstitute-ide.net', '.herokuapp.com', ]  # noqa
+ALLOWED_HOSTS = ['8000-chikayok-rateyourstay-ux2o7hwbnpz.ws.codeinstitute-ide.net', '.herokuapp.com', ]  # noqa
 
 # Application definition
 
